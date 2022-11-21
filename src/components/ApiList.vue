1198244e7b5e476efda37cd2866d0d72a6773d68
castenettoa <castenetto.andrea@gmail.com>
2022-11-21 19:30:05 +0100

    add latecy simulation and API loading effect

--- src/components/ApiList.vue
+++ src/components/ApiList.vue
@@ -3,7 +3,7 @@
   <div class="container">
     <h2>Rover API list</h2>
   <p class="subtitle text-slate-600">La lista delle API pubblicamente disponibile del Rover.</p>
-  <ul class="mt-5">
+  <ul class="mt-5" v-if="roverAPIs.length">
     <li v-for="api in roverAPIs" class="bg-slate-100 mb-2 p-2 hover:bg-slate-200">
       <router-link :to="api.path">
         <b class="underline">{{ api.path }}</b> 
@@ -12,6 +12,23 @@
       — <i>{{ api.description }}</i>
     </li>
   </ul>
+  
+  <div v-else role="status" class="max-w-sm animate-pulse my-4">
+      <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+      <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4 mt-8"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+      <span class="sr-only">Loading...</span>
+  </div>
+
   </div>
 </template>
 
@@ -22,13 +39,7 @@ export default {
   name: 'DisplayApis',
   data() {
     return {
-      roverAPIs: [
-        {
-          path: '/roverInfo',
-          type: 'get',
-          desc: 'ritorna certe informazioni sul rover',
-        }
-      ]
+      roverAPIs: []
     }
   },
 
@@ -41,7 +52,9 @@ export default {
   },
 
   async mounted() {
-    await this.fetchRoverAPisList();
+    setTimeout(async () => { // todo: remove fake latency
+      await this.fetchRoverAPisList();
+    }, 1000);
   }
 }
 </script>
